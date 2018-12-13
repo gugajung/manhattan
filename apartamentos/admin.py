90842d730f9d7142c28dbe33c0e826424b9b5b4b
jung <gugajung@gmail.com>
2018-12-13 12:56:06 -0200

    modificacoes commit

--- apartamentos/admin.py
+++ apartamentos/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
-from .models import Apartamentos, Hotel
+from .models import Apartamentos, Hotel, Tag
 # Register your models here.
 
 admin.site.register(Apartamentos)
 admin.site.register(Hotel)
+admin.site.register(Tag)
+
--- apartamentos/models.py
+++ apartamentos/models.py
@@ -6,13 +6,20 @@ class Hotel(models.Model):
     name = models.ForeignKey(Juridica, on_delete=models.CASCADE)
 
     def __str__(self):
-        return self.name
+        return str(self.name)
+
+class Tag(models.Model):
+    tag = models.CharField(max_length=30, unique=True)
+
+    def __str__(self):
+        return self.tag
 
 
 class Apartamentos(models.Model):
-    numero_apto = models.IntegerField()
-    ramal = models.IntegerField()
-    pertence = models.ForeignKey(Hotel, on_delete=models.CASCADE)
+    numero_apto = models.IntegerField(unique=True)
+    ramal = models.IntegerField(blank=True, null=True)
+    pertence = models.ForeignKey(Hotel, on_delete=models.PROTECT)
+    tag = models.ForeignKey(Tag,on_delete=models.PROTECT, blank=True, null=True )
 
     def __str__(self):
         return str(self.numero_apto)
