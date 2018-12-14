574c0d179b5c5dc41dac8015165047b7666f75ba
Gustavo jung <gugajung@gmail.com>
2018-12-14 15:05:09 -0200

    implem

--- reservas/models.py
+++ reservas/models.py
@@ -17,22 +17,35 @@ class Reservation(models.Model):
     valor_diaria = models.DecimalField(max_digits=6, decimal_places=2)
     valor_pago = models.DecimalField(max_digits=6, decimal_places=2, blank=True, null=True)
 
-    def Calcula(self):
-        checkin = datetime.datetime.strptime(self.start_date, "%m/%d/%Y").date()
-        checkout = datetime.datetime.strptime(self.end_date, "%m/%d/%Y").date()
-        custo = self.valor_diaria
-        timedeltaSum = checkout - checkin
-        StayDuration = timedeltaSum.days
+    from datetime import date
 
-        TotalCost = StayDuration * 10
-        TotalCost = int(TotalCost)
-        return TotalCost
+    def diffDate(self, data1, data2):
+        d1 = data1
+        d2 = data2
+        delta = d2 - d1
+        r = delta.days if (delta.days > 0) else "erro"
+        return r
 
+    def total_diarias(self, dias, valordiara):
+        dia = dias
+        valor = valordiara
+        valor_total = valor * dia
+        return valor_total
+
+    def saldo(self, pg, total):
+        pago = pg
+        total = total
+        saldo = total - pago
+        return saldo
 
     def __str__(self):
         return ('Apartamento: ' + str(self.room) +
                 ' - Check in: ' + str(self.start_date) +
-                ' - Check out: ' + str(self.Calcula())
+                ' - Check out: ' + str(self.end_date) +
+                ' - diárias: ' + str(self.diffDate(self.start_date, self.end_date)) +
+                ' - Valor Total: ' + str(self.total_diarias(self.diffDate(self.start_date, self.end_date), self.valor_diaria)) +
+                ' - Saldo: ' + str(self.saldo(self.valor_pago, self.total_diarias(self.diffDate(self.start_date, self.end_date), self.valor_diaria)))
+
         )
 
     class Meta:
