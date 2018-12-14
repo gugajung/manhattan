bf2a06db55f0c573eb4b6de16c46a7f28875046d
jung <gugajung@gmail.com>
2018-12-14 12:59:21 -0200

    modificacoes commit

--- reservas/admin.py
+++ reservas/admin.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
-from .models import Reservation
+from .models import Reservation, Mensalista, MovMensalista
 
 # Register your models here.
 
 admin.site.register(Reservation)
+admin.site.register(Mensalista)
+admin.site.register(MovMensalista)
+
--- reservas/models.py
+++ reservas/models.py
@@ -1,10 +1,11 @@
 from django.db import models
 import datetime
+from decimal import Decimal
+
 from apartamentos.models import Apartamentos
 
+
 # Create your models here.
-
-
 
 
 class Reservation(models.Model):
@@ -16,21 +17,42 @@ class Reservation(models.Model):
     valor_diaria = models.DecimalField(max_digits=6, decimal_places=2)
     valor_pago = models.DecimalField(max_digits=6, decimal_places=2, blank=True, null=True)
 
-    def total(self):
-        date1 = self.end_date
-        date2 = self.start_date
-        dias = abs((date1 - date2).days)
-        print(dias)
-        return self.valor_diaria * dias
+    def Calcula(self):
+        checkin = datetime.datetime.strptime(self.start_date, "%m/%d/%Y").date()
+        checkout = datetime.datetime.strptime(self.end_date, "%m/%d/%Y").date()
+        custo = self.valor_diaria
+        timedeltaSum = checkout - checkin
+        StayDuration = timedeltaSum.days
 
-    def saldo(self):
-        x = self.total - self.valor_pago
-        return str(x)
+        TotalCost = StayDuration * 10
+        TotalCost = int(TotalCost)
+        return TotalCost
+
 
     def __str__(self):
-        return 'Apartamento: ' + str(self.room) + ' - Check in: ' + str(self.start_date) + ' - Saldo a pagar: ' + self.saldo + " " + self.total
+        return ('Apartamento: ' + str(self.room) +
+                ' - Check in: ' + str(self.start_date) +
+                ' - Check out: ' + str(self.Calcula())
+        )
 
     class Meta:
         verbose_name = "Reserva"
         verbose_name_plural = "Reservas"
 
+
+class Mensalista(models.Model):
+    apto = models.ForeignKey(Apartamentos, on_delete=models.PROTECT)
+    inicio = models.DateField()
+    valor_mes = models.DecimalField(max_digits=6, decimal_places=2)
+
+    def __str__(self):
+        return str(self.apto) + ' - ' + str(self.inicio)
+
+
+class MovMensalista(models.Model):
+    mensalista = models.ForeignKey(Mensalista, on_delete=models.PROTECT)
+    dt_pgto = models.DateField()
+    total = models.DecimalField(max_digits=6, decimal_places=2)
+
+    def __str__(self):
+        return str(self.mensalista) + ' - ' + str(self.total)
